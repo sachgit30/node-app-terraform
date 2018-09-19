3ead4e721ba001022a3a71f6dd0e55106c954261
sachgit30 <sachin.2.kulkarni@atos.net>
2018-09-19 18:58:56 +0530

    Update node-app.tf

--- node-app.tf
+++ node-app.tf
@@ -4,7 +4,7 @@ variable "secret_key" {}
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "ap-south-1"
+  region     = "us-west-2"
 }
 
 data "aws_ami" "node_app_ami" {
